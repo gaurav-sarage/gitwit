38305648187b5e4b4e63ce10a7e03173725ce01e
Yash Jaiswal <bitorsic@protonmail.com>
2024-03-03 01:34:16 +0530

    Get Projects Done

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -1,8 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server'
 import db from "../../../firebase/config";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDocs, collection } from "firebase/firestore";
 
-// Add a new document in collection "cities"
+async function GET(req) {
+    try {
+      const collectionRef = collection(db, 'projects'); // Reference the "projects" collection
+  
+      const querySnapshot = await getDocs(collectionRef); // Fetch all documents
+  
+      const projects = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data() // Extract data from each document
+      }));
+  
+      return NextResponse.json(projects); // Return all projects as JSON
+    } catch (error) {
+      console.error('Error fetching projects:', error);
+      return NextResponse.json({ error: 'Failed to fetch projects' });
+    }
+  }
 
 async function POST(req) {
     const obj = await req.json();
@@ -17,5 +33,6 @@ async function POST(req) {
 }
 
 export {
+    GET,
     POST
 }
