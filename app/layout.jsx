a95f1c2a8f84c205ab0aa610f3dfffd6295cc8e0
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-02 17:28:36 +0530

    Modified the metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,8 +7,8 @@ import Header from './components/Header';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Clerk App',
-  description: 'Example Clerk App',
+  title: 'GitWit',
+  description: 'Revolutionizing Open-Source Collaboration',
 };
 
 export default function RootLayout({ children }) {
