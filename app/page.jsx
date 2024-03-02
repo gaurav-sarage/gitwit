ccd508e118958f0d332eac87b615f3521b85c6bd
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-02 17:29:02 +0530

    Basic index page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className='text-2xl font-bold mb-5'>Welcome</h1>
-      <p className='mb-5'>
-        Index page``
-      </p>
+      <div>
+        <p>Welcome to the home page!</p>
+      </div>
     </>
   );
 }
