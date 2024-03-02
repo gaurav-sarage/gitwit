430233f2f1cc5aa5e8d1135ae04459d4a21e6550
Yash Jaiswal <bitorsic@protonmail.com>
2024-03-03 00:41:22 +0530

    Add Project Done

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -1,19 +1,21 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import db from "../../../firebase/config";
 import { doc, setDoc } from "firebase/firestore";
 
 // Add a new document in collection "cities"
 
-async function GET(req) {
+async function POST(req) {
+    const obj = await req.json();
+    console.log(obj);
 
-    await setDoc(doc(db, "cities", "LA"), {
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA"
+    await setDoc(doc(db, "projects", obj.username + "-" + obj.projectName ), {
+        owner: obj.username,
+        title: obj.title,
+        desc: obj.desc
     });
     return NextResponse.json({ status: "success" })
 }
 
 export {
-    GET
+    POST
 }
