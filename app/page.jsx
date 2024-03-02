ca5143462df9ec24b6d913c885abe668d5ebea4a
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-02 19:28:35 +0530

    Imported components

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
     <>
       <div>
+        <Hero />
+        <Features />
+        <Footer />
       </div>
     </>
   );
