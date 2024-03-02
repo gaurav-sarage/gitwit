fb940ba6b50472f9b9cdfc77a2ae6c0296ef9eb2
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-02 23:44:53 +0530

    Created a simple dashboard page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const Dashboard = () => {
   return (
     <>
       <h1 className='text-2xl font-bold mb-5'>Dashboard</h1>
       <p className='mb-5'>Welcome to the dashboard!</p>
+      <div>
+      <Link href="/profile" className="mt-2 mb-2">
+        <Button variant="destructive" className="w-full">
+          Profile
+        </Button>
+      </Link>
+
+      </div>
     </>
   );
 };
