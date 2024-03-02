c60d28fef568682512c60be4d305e3be526260dd
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-02 19:25:53 +0530

    Added routes in middleware file

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/profile', '/register'],
+  publicRoutes: ["/", "/about", "/projects", "/trending-projects", "/community"],
 });
 
 export const config = {
