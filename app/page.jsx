4e69307b8183efb2317c42d90510bc77d20edc8c
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-02 16:43:20 +0530

    Updated README file

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,7 @@ export default function Home() {
     <>
       <h1 className='text-2xl font-bold mb-5'>Welcome</h1>
       <p className='mb-5'>
-        This is the demo site for Traversy Media's Next.js & Clerk tutorial. Go
-        ahead and sign up or sign in!
+        Index page``
       </p>
     </>
   );
