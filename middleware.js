b24f1dfb4325c5c5bff50f700b4e12c9deaf971a
Soham Kudalkar <sohamskudalkar02@gmail.com>
2024-03-03 05:56:49 +0530

    tested new api and removed few commented lines of code

--- middleware.js
+++ middleware.js
@@ -5,7 +5,6 @@ export default authMiddleware({
 });
 
 export const config = {
-  // matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
   matcher: ['/((?!.*\\..*|_next).*)']
 };
 // matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
