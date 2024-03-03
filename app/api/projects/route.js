8d28e926d4c950d84b2ccb4ccbb9e84e714d5feb
Soham Kudalkar <sohamskudalkar02@gmail.com>
2024-03-03 05:45:59 +0530

    added way to add tags in create projects

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -4,30 +4,30 @@ import { doc, setDoc, getDocs, collection } from "firebase/firestore";
 
 async function GET(req) {
     try {
-      const collectionRef = collection(db, 'projects'); // Reference the "projects" collection
-  
-      const querySnapshot = await getDocs(collectionRef); // Fetch all documents
-  
-      const projects = querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data() // Extract data from each document
-      }));
-  
-      return NextResponse.json(projects); // Return all projects as JSON
+        const collectionRef = collection(db, 'projects'); // Reference the "projects" collection
+
+        const querySnapshot = await getDocs(collectionRef); // Fetch all documents
+
+        const projects = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data() // Extract data from each document
+        }));
+
+        return NextResponse.json(projects); // Return all projects as JSON
     } catch (error) {
-      console.error('Error fetching projects:', error);
-      return NextResponse.json({ error: 'Failed to fetch projects' });
+        console.error('Error fetching projects:', error);
+        return NextResponse.json({ error: 'Failed to fetch projects' });
     }
-  }
+}
 
 async function POST(req) {
     const obj = await req.json();
-    console.log(obj);
 
-    await setDoc(doc(db, "projects", obj.username + "-" + obj.projectName ), {
-        owner: obj.username,
-        title: obj.title,
-        desc: obj.desc
+    await setDoc(doc(db, "projects", obj.username + "-" + obj.projectName), {
+        owner: obj.username,    // string
+        title: obj.title,   // string
+        desc: obj.desc,     // string
+        tags: obj.tags      // [string]
     });
     return NextResponse.json({ status: "success" })
 }
